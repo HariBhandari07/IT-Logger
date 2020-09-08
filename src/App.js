860bbdb60cf37ef133e9a30599becc7bb5a9f3b5
HariBhandari07 <bhandarihari07@gmail.com>
2020-09-08 20:44:49 +0545

    Materialize Setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css/dist/js/materialize.min'
 
 function App() {
+    useEffect(() => {
+        // Init Materialize JS
+        M.AutoInit()
+    })
     return (
         <div className="App">
             Hari Bhandari
