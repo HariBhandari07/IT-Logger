131597e3be51912c26f9883ab434ebf041b72d5b
HariBhandari07 <bhandarihari07@gmail.com>
2020-09-08 19:05:49 +0545

    Initialize project using Create React App, get rid of unnecessary files and install some dependencies

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            Hari Bhandari
+        </div>
+    );
+}
+
+export default App;
