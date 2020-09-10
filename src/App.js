521a4d4788d9911159cb1ef6d5bd9dcde342cdba
HariBhandari07 <bhandarihari07@gmail.com>
2020-09-10 20:29:45 +0545

    Redux store and Provider initial setup

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import AddLogModal from './components/logs/AddLogModal'
 import EditLogModal from './components/logs/Edit LogModal'
 import AddTechModal from './components/techs/AddTechModal'
 import TechListModal from './components/techs/TechListModal'
+import { Provider } from 'react-redux'
+import store from './store'
 
 function App() {
     useEffect(() => {
@@ -16,18 +18,20 @@ function App() {
         M.AutoInit()
     })
     return (
-        <Fragment>
-            <Searchbar />
-            <div className="container">
-                <AddBtn />
-                <AddLogModal />
-                <EditLogModal />
-                <AddTechModal />
-                <TechListModal />
-                <Logs />
+        <Provider store={store}>
+            <Fragment>
+                <Searchbar />
+                <div className="container">
+                    <AddBtn />
+                    <AddLogModal />
+                    <EditLogModal />
+                    <AddTechModal />
+                    <TechListModal />
+                    <Logs />
 
-            </div>
-        </Fragment>
+                </div>
+            </Fragment>
+        </Provider>
     )
 }
 
